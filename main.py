0a0a7a9e08a5b4ff7bdefde60191d50ff53ef418
Vanshika Sarawgi <vanshikasarawgi@gmail.com>
2025-11-13 17:02:26 +0530

    Create weather data dataframe

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 from dotenv import load_dotenv
 import os
+import pandas as pd
+from datetime import datetime,timezone
+import pytz
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 load_dotenv()
 
@@ -22,6 +27,26 @@ response.raise_for_status()
 data = response.json()
 
 forecast_list = data['list']
-print(forecast_list)
 
+weather_data = []
+
+#create  the dataframe
+
+for item in forecast_list:
+
+    utc_time = datetime.fromtimestamp(item['dt'], tz=timezone.utc)  # UTC timezone-aware
+    ist_time = utc_time.astimezone(pytz.timezone('Asia/Kolkata'))   # Convert to IST
+
+    weather_data.append({
+        'time': ist_time,
+        'temp': item['main']['temp'],
+        'min_temp': item['main']['temp_min'],
+        'max_temp': item['main']['temp_max'],
+        'pressure': item['main']['pressure'],
+        'humidity': item['main']['humidity'],
+        'sea_level': item['main']['sea_level']
+    })
+
+df = pd.DataFrame(weather_data)
+print(df)
 
